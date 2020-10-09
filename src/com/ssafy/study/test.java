a9072c884e2a9dc23952618592c96dca23710684
DESKTOP-0F2333F\elwla <elwlahd555@naver.com>
2020-10-09 17:33:40 +0900

    no message

--- src/com/ssafy/study/test.java
+++ src/com/ssafy/study/test.java
@@ -1,10 +1,18 @@
 package com.ssafy.study;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class test {
 	public static void main(String[] args) {
 		LinkedList<Integer> que=new LinkedList<Integer>();
-		
+		que.add(1);
+		que.add(2);
+		que.add(3);
+		que.add(4);
+		que.pollLast();
+		for (int i = 0; i < que.size(); i++) {
+			System.out.println(que.get(i));
+		}
 	}
 }
