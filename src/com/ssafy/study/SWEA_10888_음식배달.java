19fdfe58bbf2e5d2d7ccac8bf9eb69dca3fc61b0
DESKTOP-0F2333F\elwla <elwlahd555@naver.com>
2020-12-03 21:29:03 +0900

    no message

--- src/com/ssafy/study/SWEA_10888_음식배달.java
+++ src/com/ssafy/study/SWEA_10888_음식배달.java
@@ -6,11 +6,12 @@ import java.util.Scanner;
 public class SWEA_10888_음식배달 {
 	private static int arr[][];
 	private static ArrayList<Point> temp;
-	
+
 	private static int N, count;
 	private static boolean visit[];
-	static class Point{
-		int x,y;
+
+	static class Point {
+		int x, y;
 
 		public Point(int x, int y) {
 			super();
@@ -18,65 +19,68 @@ public class SWEA_10888_음식배달 {
 			this.y = y;
 		}
 	}
+
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int T=sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
 		for (int t = 1; t <= T; t++) {
-			N=sc.nextInt();
-			arr=new int[N][N];
-			count=0;
-			temp=new ArrayList<Point>();
+			N = sc.nextInt();
+			arr = new int[N][N];
+			count = 0;
+			temp = new ArrayList<Point>();
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					arr[i][j]=sc.nextInt();
-					if(arr[i][j]>=2) {
-						temp.add(new Point(i,j));
+					arr[i][j] = sc.nextInt();
+					if (arr[i][j] >= 2) {
+						temp.add(new Point(i, j));
 						count++;
 					}
 				}
-				
+
 			}
-			visit=new boolean[count];
-			result=Integer.MAX_VALUE;
+			visit = new boolean[count];
+			result = Integer.MAX_VALUE;
 			dfs(0);
-			System.out.println("#"+t+" "+result);
-			
+			System.out.println("#" + t + " " + result);
+
 		}
 	}
+
 	private static int result;
+
 	private static void dfs(int cnt) {
 		// TODO Auto-generated method stub
-		if(cnt==count) {
-			int tempcal=cal();
-			if(result>tempcal&&tempcal>0) {
-				result=tempcal;
+		if (cnt == count) {
+			int tempcal = cal();
+			if (result > tempcal && tempcal > 0) {
+				result = tempcal;
 			}
 			return;
 		}
-		visit[cnt]=true;
-		dfs(cnt+1);
-		visit[cnt]=false;
-		dfs(cnt+1);
+		visit[cnt] = true;
+		dfs(cnt + 1);
+		visit[cnt] = false;
+		dfs(cnt + 1);
 	}
+
 	private static int cal() {
 		// TODO Auto-generated method stub
-		int sum=0;
+		int sum = 0;
 		for (int i = 0; i < count; i++) {
-			if(visit[i]) {
-				sum+=arr[temp.get(i).x][temp.get(i).y];
+			if (visit[i]) {
+				sum += arr[temp.get(i).x][temp.get(i).y];
 			}
 		}
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				int sumtemp=Integer.MAX_VALUE;
-				if(arr[i][j]==1) {
+				int sumtemp = Integer.MAX_VALUE;
+				if (arr[i][j] == 1) {
 					for (int j2 = 0; j2 < count; j2++) {
-						if(visit[j2]&&sumtemp>Math.abs(temp.get(j2).x-i)+Math.abs(temp.get(j2).y-j)
-					) {
-							sumtemp=Math.abs(temp.get(j2).x-i)+Math.abs(temp.get(j2).y-j);
+						if (visit[j2] && sumtemp > Math.abs(temp.get(j2).x - i) + Math.abs(temp.get(j2).y - j)) {
+							sumtemp = Math.abs(temp.get(j2).x - i) + Math.abs(temp.get(j2).y - j);
 						}
 					}
-					sum+=sumtemp;
+					sum += sumtemp;
 				}
 			}
 		}
--- src/com/ssafy/study/SWEA_1249_보급로.java
+++ src/com/ssafy/study/SWEA_1249_보급로.java
@@ -7,53 +7,52 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class SWEA_1249_보급로 {
-	private static class Point{
-		int x,y;
+	private static class Point {
+		int x, y;
 
 		public Point(int x, int y) {
 			super();
 			this.x = x;
 			this.y = y;
 		}
-		
+
 	}
-	private static int dx[]= {-1,1,0,0};
-	private static int dy[]= {0,0,-1,1};
-	
+
+	private static int dx[] = { -1, 1, 0, 0 };
+	private static int dy[] = { 0, 0, -1, 1 };
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(bf.readLine());
-		int T=Integer.parseInt(st.nextToken());
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int T = Integer.parseInt(st.nextToken());
 		for (int t = 1; t <= T; t++) {
-			st=new StringTokenizer(bf.readLine());
-			int N=Integer.parseInt(st.nextToken());
-			int arr[][]=new int[N][N];
-			int visit[][]=new int[N][N];
+			st = new StringTokenizer(bf.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int arr[][] = new int[N][N];
+			int visit[][] = new int[N][N];
 			for (int i = 0; i < N; i++) {
-				String temp=bf.readLine();
+				String temp = bf.readLine();
 				for (int j = 0; j < N; j++) {
-					arr[i][j]=temp.charAt(j)-'0';
-					visit[i][j]=Integer.MAX_VALUE;
+					arr[i][j] = temp.charAt(j) - '0';
+					visit[i][j] = Integer.MAX_VALUE;
 				}
 			}
-			visit[0][0]=0;
-			LinkedList<Point> que=new LinkedList<Point>();
-			que.add(new Point(0,0));
-			while(!que.isEmpty()) {
-				Point point=que.poll();
+			visit[0][0] = 0;
+			LinkedList<Point> que = new LinkedList<Point>();
+			que.add(new Point(0, 0));
+			while (!que.isEmpty()) {
+				Point point = que.poll();
 				for (int k = 0; k < 4; k++) {
-					int x=point.x+dx[k];
-					int y=point.y+dy[k];
-					if(x>=0&&x<N&&y>=0&&y<N&&visit[point.x][point.y]+arr[x][y]<visit[x][y]) {
-						visit[x][y]=visit[point.x][point.y]+arr[x][y];
-						que.add(new Point(x,y));
+					int x = point.x + dx[k];
+					int y = point.y + dy[k];
+					if (x >= 0 && x < N && y >= 0 && y < N && visit[point.x][point.y] + arr[x][y] < visit[x][y]) {
+						visit[x][y] = visit[point.x][point.y] + arr[x][y];
+						que.add(new Point(x, y));
 					}
 				}
 			}
-			System.out.println("#"+t+" "+visit[N-1][N-1]);
-					
+			System.out.println("#" + t + " " + visit[N - 1][N - 1]);
+
 		}
 	}
 }
-
-
