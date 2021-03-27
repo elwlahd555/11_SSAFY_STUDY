02f5187d5bfed9b5af63428a688740abe0ad3692
DESKTOP-KVCQHCD\multicampus <ghqls0702@naver.com>
2021-03-27 13:42:28 +0900

    no message

---
+++ src/com/c_algorithm/BJ/사칙연산.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int num1;
+    int num2;
+    cin>>num1;
+    cin>>num2;
+    cout<<num1+num2 <<endl;
+    cout<<num1-num2 <<endl;
+    cout<<num1*num2 <<endl;
+    cout<<num1/num2 <<endl;
+    cout<<num1%num2 <<endl;
+    return 0;
+}
